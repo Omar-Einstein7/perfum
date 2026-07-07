5d37457b6fd7be8f6378d070c1e94c373e1d6933
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-08 01:43:11 +0300

    feat(units): update UnitRemoteDataSource with FutureEither and full CRUD

--- lib/src/features/units/data/datasources/unit_remote_data_source.dart
+++ lib/src/features/units/data/datasources/unit_remote_data_source.dart
@@ -1,13 +1,16 @@
+import 'package:fpdart/fpdart.dart' hide Unit;
+import 'package:perfum_ahmed_gaper/src/utils/utils.dart';
 import 'package:perfum_ahmed_gaper/src/services/dio_service.dart';
 import '../models/unit_list_response.dart';
 import '../models/unit_model.dart';
+import '../../domain/entities/unit.dart';
 
 abstract class UnitRemoteDataSource {
-  Future<UnitListResponse> list({int page = 1, int limit = 20, String search = ''});
-  Future<UnitModel> getById(String id);
-  Future<UnitModel> create(String name);
-  Future<UnitModel> update(String id, String name);
-  Future<void> delete(String id);
+  FutureEither<UnitListResponse> listUnits({int page = 1, int limit = 20, String? search});
+  FutureEither<UnitModel> getUnit(String id);
+  FutureEither<UnitModel> createUnit({required String name, required String abbreviation, required UnitType type, String? description});
+  FutureEither<UnitModel> updateUnit({required String id, String? name, String? abbreviation, UnitType? type, String? description});
+  FutureEither<void> deleteUnit(String id);
 }
 
 class UnitRemoteDataSourceImpl implements UnitRemoteDataSource {
@@ -16,51 +19,60 @@ class UnitRemoteDataSourceImpl implements UnitRemoteDataSource {
   UnitRemoteDataSourceImpl({required this.dio});
 
   @override
-  Future<UnitListResponse> list({int page = 1, int limit = 20, String search = ''}) async {
+  FutureEither<UnitListResponse> listUnits({int page = 1, int limit = 20, String? search}) async {
     final params = <String, dynamic>{'page': page, 'limit': limit};
-    if (search.isNotEmpty) {
+    if (search != null && search.isNotEmpty) {
       params['search'] = search;
     }
-    final result = await dio.get('/api/units', queryParameters: params);
-    return result.fold(
-      (failure) => throw failure,
-      (response) => UnitListResponse.fromJson(response.data as Map<String, dynamic>),
-    );
+    final result = await dio.get('/units', queryParameters: params);
+    return result.map((response) {
+      return UnitListResponse.fromJson(response.data as Map<String, dynamic>);
+    });
   }
 
   @override
-  Future<UnitModel> getById(String id) async {
-    final result = await dio.get('/api/units/$id');
-    return result.fold(
-      (failure) => throw failure,
-      (response) => UnitModel.fromJson(response.data['data'] as Map<String, dynamic>),
-    );
+  FutureEither<UnitModel> getUnit(String id) async {
+    final result = await dio.get('/units/$id');
+    return result.map((response) {
+      final data = response.data is Map<String, dynamic> ? response.data['data'] ?? response.data : response.data;
+      return UnitModel.fromJson(data as Map<String, dynamic>);
+    });
   }
 
   @override
-  Future<UnitModel> create(String name) async {
-    final result = await dio.post('/api/units', data: {'name': name});
-    return result.fold(
-      (failure) => throw failure,
-      (response) => UnitModel.fromJson(response.data['data'] as Map<String, dynamic>),
-    );
+  FutureEither<UnitModel> createUnit({required String name, required String abbreviation, required UnitType type, String? description}) async {
+    final result = await dio.post('/units', data: {
+      'name': name,
+      'abbreviation': abbreviation,
+      'type': type.toJson(),
+      if (description != null) 'description': description,
+    });
+    return result.map((response) {
+      final data = response.data is Map<String, dynamic> ? response.data['data'] ?? response.data : response.data;
+      return UnitModel.fromJson(data as Map<String, dynamic>);
+    });
   }
 
   @override
-  Future<UnitModel> update(String id, String name) async {
-    final result = await dio.put('/api/units/$id', data: {'name': name});
-    return result.fold(
-      (failure) => throw failure,
-      (response) => UnitModel.fromJson(response.data['data'] as Map<String, dynamic>),
-    );
+  FutureEither<UnitModel> updateUnit({required String id, String? name, String? abbreviation, UnitType? type, String? description}) async {
+    final body = <String, dynamic>{};
+    if (name != null) body['name'] = name;
+    if (abbreviation != null) body['abbreviation'] = abbreviation;
+    if (type != null) body['type'] = type.toJson();
+    if (description != null) body['description'] = description;
+    final result = await dio.put('/units/$id', data: body);
+    return result.map((response) {
+      final data = response.data is Map<String, dynamic> ? response.data['data'] ?? response.data : response.data;
+      return UnitModel.fromJson(data as Map<String, dynamic>);
+    });
   }
 
   @override
-  Future<void> delete(String id) async {
-    final result = await dio.delete('/api/units/$id');
+  FutureEither<void> deleteUnit(String id) async {
+    final result = await dio.delete('/units/$id');
     return result.fold(
-      (failure) => throw failure,
-      (_) => null,
+      (failure) => Future.value(left<Failure, void>(failure)),
+      (_) => right<Failure, void>(null),
     );
   }
 }
