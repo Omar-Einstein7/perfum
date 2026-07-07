90aa653e7585a930ba7a45bc26ce908778088d90
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-08 01:43:00 +0300

    feat(units): update CreateUnitUseCase with named params (name, abbreviation, type, description)

--- lib/src/features/units/domain/usecases/create_unit_usecase.dart
+++ lib/src/features/units/domain/usecases/create_unit_usecase.dart
@@ -1,11 +1,17 @@
 import 'package:fpdart/fpdart.dart' hide Unit;
 import 'package:perfum_ahmed_gaper/src/utils/failure.dart';
-import '../entities/unit.dart';
 import '../repositories/unit_repository.dart';
+import '../entities/unit.dart';
 
 class CreateUnitUseCase {
   final UnitRepository repository;
   CreateUnitUseCase({required this.repository});
 
-  Future<Either<Failure, Unit>> call(String name) => repository.create(name);
+  Future<Either<Failure, Unit>> call({
+    required String name,
+    required String abbreviation,
+    required UnitType type,
+    String? description,
+  }) =>
+      repository.createUnit(name: name, abbreviation: abbreviation, type: type, description: description);
 }
