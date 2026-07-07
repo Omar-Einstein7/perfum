499e6582ce0562acebc34c52f3fb4aa32bd749e1
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-08 01:43:01 +0300

    feat(units): update UpdateUnitUseCase with named params

--- lib/src/features/units/domain/usecases/update_unit_usecase.dart
+++ lib/src/features/units/domain/usecases/update_unit_usecase.dart
@@ -1,12 +1,18 @@
 import 'package:fpdart/fpdart.dart' hide Unit;
 import 'package:perfum_ahmed_gaper/src/utils/failure.dart';
-import '../entities/unit.dart';
 import '../repositories/unit_repository.dart';
+import '../entities/unit.dart';
 
 class UpdateUnitUseCase {
   final UnitRepository repository;
   UpdateUnitUseCase({required this.repository});
 
-  Future<Either<Failure, Unit>> call(String id, String name) =>
-      repository.update(id, name);
+  Future<Either<Failure, Unit>> call({
+    required String id,
+    String? name,
+    String? abbreviation,
+    UnitType? type,
+    String? description,
+  }) =>
+      repository.updateUnit(id: id, name: name, abbreviation: abbreviation, type: type, description: description);
 }
