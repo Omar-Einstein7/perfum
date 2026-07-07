e844f1b76f9b3a5fc21111da6e0794afa3a7e8fc
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-08 01:43:00 +0300

    feat(units): update GetUnitUseCase

--- lib/src/features/units/domain/usecases/get_unit_usecase.dart
+++ lib/src/features/units/domain/usecases/get_unit_usecase.dart
@@ -1,11 +1,11 @@
 import 'package:fpdart/fpdart.dart' hide Unit;
 import 'package:perfum_ahmed_gaper/src/utils/failure.dart';
-import '../entities/unit.dart';
 import '../repositories/unit_repository.dart';
+import '../entities/unit.dart';
 
 class GetUnitUseCase {
   final UnitRepository repository;
   GetUnitUseCase({required this.repository});
 
-  Future<Either<Failure, Unit>> call(String id) => repository.getById(id);
+  Future<Either<Failure, Unit>> call(String id) => repository.getUnit(id);
 }
