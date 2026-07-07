7a480e7cecc892abb14b87c701ce6dcc3deb705e
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-08 01:42:51 +0300

    feat(units): update UnitRepository interface with named params and PaginatedResponse

--- lib/src/features/units/domain/repositories/unit_repository.dart
+++ lib/src/features/units/domain/repositories/unit_repository.dart
@@ -1,25 +1,31 @@
 import 'package:fpdart/fpdart.dart' hide Unit;
 import 'package:perfum_ahmed_gaper/src/utils/failure.dart';
 import '../entities/unit.dart';
-
-class PaginatedUnits {
-  final List<Unit> units;
-  final int total;
-  final int page;
-  final int pages;
-
-  const PaginatedUnits({
-    required this.units,
-    required this.total,
-    required this.page,
-    required this.pages,
-  });
-}
+import '../entities/paginated_response.dart';
 
 abstract class UnitRepository {
-  Future<Either<Failure, PaginatedUnits>> list({int page = 1, int limit = 20, String search = ''});
-  Future<Either<Failure, Unit>> getById(String id);
-  Future<Either<Failure, Unit>> create(String name);
-  Future<Either<Failure, Unit>> update(String id, String name);
-  Future<Either<Failure, void>> delete(String id);
+  Future<Either<Failure, PaginatedResponse<Unit>>> listUnits({
+    int page = 1,
+    int limit = 20,
+    String? search,
+  });
+
+  Future<Either<Failure, Unit>> getUnit(String id);
+
+  Future<Either<Failure, Unit>> createUnit({
+    required String name,
+    required String abbreviation,
+    required UnitType type,
+    String? description,
+  });
+
+  Future<Either<Failure, Unit>> updateUnit({
+    required String id,
+    String? name,
+    String? abbreviation,
+    UnitType? type,
+    String? description,
+  });
+
+  Future<Either<Failure, void>> deleteUnit(String id);
 }
