0168b5445e54490387e63be1f8c6765908780f44
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-08 01:42:59 +0300

    feat(units): update ListUnitsUseCase with named params

--- lib/src/features/units/domain/usecases/list_units_usecase.dart
+++ lib/src/features/units/domain/usecases/list_units_usecase.dart
@@ -1,11 +1,17 @@
 import 'package:fpdart/fpdart.dart' hide Unit;
 import 'package:perfum_ahmed_gaper/src/utils/failure.dart';
 import '../repositories/unit_repository.dart';
+import '../entities/paginated_response.dart';
+import '../entities/unit.dart';
 
 class ListUnitsUseCase {
   final UnitRepository repository;
   ListUnitsUseCase({required this.repository});
 
-  Future<Either<Failure, PaginatedUnits>> call({int page = 1, int limit = 20, String search = ''}) =>
-      repository.list(page: page, limit: limit, search: search);
+  Future<Either<Failure, PaginatedResponse<Unit>>> call({
+    int page = 1,
+    int limit = 20,
+    String? search,
+  }) =>
+      repository.listUnits(page: page, limit: limit, search: search);
 }
